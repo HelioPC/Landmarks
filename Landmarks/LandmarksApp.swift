9b85a3b06c46739b332d97b9f0455380c73485a2
Eliúde Vemba <sweeteliude@gmail.com>
2023-09-10 12:27:13 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eliude Vemba on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
