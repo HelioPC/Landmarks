273c6cb29bb7bae3cb9b7063a69a6900460cece7
Eliúde Vemba <sweeteliude@gmail.com>
2023-09-11 01:01:44 +0100

    Chapter 02 ✅

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Eliude Vemba on 11/09/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone 14"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Eliude Vemba on 11/09/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
